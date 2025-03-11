cf77cce5f13addbd9319a7d94eb29c9e36d4cc22
hanane987 <aminahanane987@gmail.com>
2025-03-11 06:46:56 +0100

    feat(EM-7): Implement the the attributes that should be cast.

--- cyberscale-starter-api-laravel-9e7ba016402f/app/Models/User.php
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 
      *
      * @var array<int, string>
      */
@@ -27,7 +27,6 @@ class User extends Authenticatable
 
     /**
      * 
-     *
      * @var array<int, string>
      */
     protected $hidden = [
@@ -35,6 +34,16 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-  
+    /**
+     * 
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    
 }
 
