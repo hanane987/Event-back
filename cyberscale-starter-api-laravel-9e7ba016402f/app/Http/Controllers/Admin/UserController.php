b29803caa4c54c21b9e18464766580710ac7695f
hanane987 <aminahanane987@gmail.com>
2025-03-11 06:54:27 +0100

    feat(EM-7): Add  Display a listing of the resource..

---
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $this->authorize('viewAny', User::class);
+        
+        $users = User::paginate(15);
+        
+        return response()->json($users);
+    }
+}
