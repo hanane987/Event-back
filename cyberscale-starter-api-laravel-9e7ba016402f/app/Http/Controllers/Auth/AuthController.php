129ed1b5c2939ad8d0052d09b9ead5464f925cd1
hanane987 <aminahanane987@gmail.com>
2025-03-11 07:02:00 +0100

    feat(EM-7): Add the code of regiser of a user

---
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /**
+     * 
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'phone' => 'nullable|string|max:20',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+            'role' => 'attendee', 
+        ]);
+
+        
+        if (config('auth.email_verification_enabled', false)) {
+            $user->sendEmailVerificationNotification();
+        }
+
+        Auth::login($user);
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user
+        ], 201);
+    }  }
