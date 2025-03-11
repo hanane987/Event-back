0aef179c5dc74ef4ed938f0ad139d5149122e0dd
hanane987 <aminahanane987@gmail.com>
2025-03-11 07:17:20 +0100

    feat(EM-7): Add the function to authorize services

--- cyberscale-starter-api-laravel-9e7ba016402f/app/Providers/AuthServiceProvider.php
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Providers/AuthServiceProvider.php
@@ -2,18 +2,29 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Category;
+use App\Models\Booking;
+use App\Policies\UserPolicy;
+use App\Policies\EventPolicy;
+use App\Policies\CategoryPolicy;
+use App\Policies\BookingPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
+     * 
      *
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
+        Event::class => EventPolicy::class,
+        Category::class => CategoryPolicy::class,
+        Booking::class => BookingPolicy::class,
     ];
 
     /**
@@ -25,6 +36,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        
+        Gate::define('viewDashboard', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
+
