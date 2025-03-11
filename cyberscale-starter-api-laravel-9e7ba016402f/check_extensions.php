56b5e4acda45ace93c35ad4e434b468c06d5c76f
hanane987 <aminahanane987@gmail.com>
2025-03-11 06:30:53 +0100

    feat(EM-7): Implement secure user registration and login

---
+++ cyberscale-starter-api-laravel-9e7ba016402f/check_extensions.php
@@ -0,0 +1,5 @@
+<?php
+var_dump(extension_loaded('gd'));
+var_dump(extension_loaded('openssl'));
+var_dump(extension_loaded('zip'));
+?>
---
+++ cyberscale-starter-api-laravel-9e7ba016402f/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get(
+    '/auth/disconnected', function () {
+        return __('auth.disconnected');
+    }
+)->name('auth.disconnected');
+
+
+Route::get('/', function () {
+    return view('welcome'); // or your desired view
+});
