ab056594cd1479a8f0efefc24adbff4cc60619c9
hanane987 <aminahanane987@gmail.com>
2025-03-11 07:03:21 +0100

    feat(EM-7): Add the code of login of a user

--- cyberscale-starter-api-laravel-9e7ba016402f/app/Http/Controllers/Auth/AuthController.php
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Http/Controllers/Auth/AuthController.php
@@ -47,4 +47,39 @@ class AuthController extends Controller
             'message' => 'User registered successfully',
             'user' => $user
         ], 201);
-    }  }
+        
+    } 
+ /**
+     * Login a user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember' => 'boolean',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        $remember = $request->remember ?? false;
+
+        if (Auth::attempt($credentials, $remember)) {
+            $request->session()->regenerate();
+            
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => Auth::user()
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'The provided credentials do not match our records.',
+        ], 401);
+    }
+
+
+
+}
