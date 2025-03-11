aeb572edbed7e03130f668e576b899f8f07efd65
hanane987 <aminahanane987@gmail.com>
2025-03-11 07:13:26 +0100

    feat(EM-7): Add the function to determine whether the user can view the model.

--- cyberscale-starter-api-laravel-9e7ba016402f/app/Policies/UserPolicy.php
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Policies/UserPolicy.php
@@ -10,7 +10,6 @@ class UserPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
      *
      * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
@@ -19,6 +18,16 @@ class UserPolicy
     {
         return $user->isAdmin();
     } 
-
+ /**
+     * 
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, User $model)
+    {
+        return $user->isAdmin() || $user->id === $model->id;
+    }
 
 }
