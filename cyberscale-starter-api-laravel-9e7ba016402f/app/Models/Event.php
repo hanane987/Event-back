374a986a0bd7ccac3e1badded660de4446ee8336
hanane987 <aminahanane987@gmail.com>
2025-03-11 22:44:01 +0100

    feat(EM-16): Add the code of model of event.

---
+++ cyberscale-starter-api-laravel-9e7ba016402f/app/Models/Event.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'location',
+        'address',
+        'capacity',
+        'status', 
+        'category', 
+        'creator_id',
+        'featured_image',
+        'is_featured',
+    ];
+
+   
+}
